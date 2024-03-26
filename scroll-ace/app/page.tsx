e84fbda5a6ed12dc0f1a7d95151ce409f5d88109
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-03-26 18:48:33 -0400

    paperplacing

--- scroll-ace/app/page.tsx
+++ scroll-ace/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroScrollDemo } from "./components/containerscroll";
 import { HeroParallaxDemo } from "./components/parallax";
 import Clayimage from '../public/ccclaymoji.png'
 import Navbar from "./components/global/navbar";
+import Paper from '../public/paper.png'
 
 export default function Home() {
   return (
@@ -10,6 +11,12 @@ export default function Home() {
     
     <div>
         <Navbar />
+        <Image 
+          src='/paper.png'
+          alt='Paper Png'
+          width={50}
+          height={50}
+        />
         <HeroScrollDemo />
         <HeroParallaxDemo />
         <div className="subsection items-center flex justify-center">
