3dcdec366e6efa500f06d7f8f8502a12cb105c9a
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-03-27 15:43:03 -0400

    Signup feature

--- scroll-ace/app/page.tsx
+++ scroll-ace/app/page.tsx
@@ -22,17 +22,28 @@ export default function Home() {
           <p className=" text-neutral-500 mt-4 text-xl">Browse from curated illustrations to templates & more..</p>
          </div>
         <div className="subsection items-center flex justify-center grid grid-cols-3 gap-10 p-10">
-          <div className="box p-10 border rounded-lg shadow-lg bg-[#ECF2F2] mb-20">
-            <div className="h1 font-bold">Web Skins</div>
-            <div className="body text-sm mt-5">Creating</div>
+          <div className="box p-10 rounded-lg shadow-sm bg-white border shadow-sm mb-20">
+          <div className="imagesec items-center flex justify-center align-middle mb-5 max-h-[700px]">
+            <Image src = '/template.png' alt = 'Custom Work'  width={200} height={10}/>
+            </div>            <div className="h1 font-bold">Web Skins</div>
+            <div className="body text-sm mt-4 text-neutral-500">Check out differet aesthetic websites that you will fall in love with, both code & nocode. </div>
+            <button className="bg-[#222222] p-2 shadow-md mt-5 text-sm rounded-lg h-full w-full text-white">View</button>
           </div>
-          <div className="box p-10 border rounded-lg shadow-lg bg-[#ECF2F2] mb-20">
-            <div className="h1 font-bold">Web Skins</div>
-            <div className="body text-sm mt-5">Creating</div>
+          <div className="p-10 box rounded-lg shadow-sm bg-white border mb-20 items-center align-middle">
+            <div className="imagesec items-center flex justify-center align-middle mb-5">
+            <Image src = '/illustration.png' alt = 'Custom Work'  width={180} height={10}/>
+            </div>
+            <div className="h1 font-bold text-lg">Illustration Packs</div>
+            <div className="body text-sm mt-4 text-neutral-500">Browse from a collection of various illustrations styles you can throw on your sites.</div>
+            <button className="bg-[#222222] p-2 shadow-md mt-5 text-sm rounded-lg h-full w-full text-white">View</button>
           </div>
-          <div className="box p-10 border rounded-lg shadow-lg bg-[#ECF2F2] mb-20">
-            <div className="h1 font-bold">Web Skins</div>
-            <div className="body text-sm mt-5">Creating</div>
+          <div className="box p-10 rounded-lg shadow-sm border bg-red-40 mb-20">
+          <div className="imagesec items-center flex justify-center align-middle mb-5">
+            <Image src = '/custom.png' alt = 'Custom Work'  width={300} height={10}/>
+            </div>
+            <div className="h1 font-bold text-lg">Custom Work</div>
+            <div className="body text-sm mt-4 text-neutral-500">Let's have a chat about what work you want done regarding your brand, website or designs.</div>
+            <button className="bg-[#222222] p-2 shadow-md mt-5 text-sm rounded-lg h-full w-full text-white">Find out more</button>
           </div>
         </div>
         </div>
