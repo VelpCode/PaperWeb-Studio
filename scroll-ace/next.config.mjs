5ead2f01d9c641d83884ee1773bde3edb31a545f
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-03-29 14:57:10 -0400

    custom svg addition

--- scroll-ace/next.config.mjs
+++ scroll-ace/next.config.mjs
@@ -1,10 +1,20 @@
 // next.config.mjs
+
+import { use } from 'react';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['aceternity.com', 'i.ibb.co', 'images.unsplash.com'],
   },
-  // ...other configurations if needed
+  webpack(config, {isServer, webpack}) {
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
