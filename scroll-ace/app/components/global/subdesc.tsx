6427ad320b82d8a54196ad21a0604703a1b14687
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-03-30 15:57:18 -0400

    20minute

--- scroll-ace/app/components/global/subdesc.tsx
+++ scroll-ace/app/components/global/subdesc.tsx
@@ -23,7 +23,18 @@ const subdesc = () => {
                 </div>
             </div>
             <div className="mt-10 flex justify-center">
-          <button className="px-10 py-5 bg-black text-white font-semibold   rounded-lg shadow-xl">Get a free strategy session</button>
+            <div class="relative mt-10">
+    <div class="absolute -inset-5">
+        <div
+            class="w-full h-full max-w-sm mx-auto lg:mx-0 opacity-30 blur-lg bg-gradient-to-r from-yellow-400 via-pink-500 to-green-600">
+        </div>
+    </div>
+    <a href="#" title=""
+        class="relative z-10 inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+        role="button">
+        Free 20 minute game-plan call
+    </a>
+</div>
         </div>
         </div>
     </div>
